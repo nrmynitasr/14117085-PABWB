d039767b415d47c9f6ed678db473c27a4ab22234
nrmynitasr <49836876+nrmynitasr@users.noreply.github.com>
2019-11-17 22:24:45 +0700

    Website with CI

---
+++ nitablog/application/views/Contact.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Contact</title>
+    <link rel="stylesheet" type="text/css" href="<?= base_url();?>asset/css/kontak.css">
+    <link rel="stylesheet" type="text/css" href="<?= base_url();?>asset/css/menu.css">
+</head>
+<body>
+    <br>
+    <nav align="center" class="menu">
+        <ul>
+            <li><a href="<?= base_url();?>Website">Home</a></li>
+            <li><a href="<?= base_url();?>Profil">Profile</a></li>
+            <li><a href="<?= base_url();?>Story">Story</a></li>
+            <li><a href="<?= base_url();?>Contact">Contact</a></li>
+        </ul>
+        <br/>
+    </nav>
+
+        <br>
+        <h2 align="center">My Contact</h2>
+        <h4 align="center">"If you want to be closer to me, contact me !" </h4>
+        <br>
+        <img src="<?= base_url();?>asset/img/email.jpg" class="imgprofile"></img>
+        <h3 align="img/center">nurmayunitasari1@gmail.com</h3>
+        <img src="<?= base_url();?>asset/img/wa.jpg" class="imgprofile"></img>
+        <h3 align="<?= base_url();?>asset/img/center">+6281368714618</h3>
+        <img src="<?= base_url();?>asset/img/line.jpg" class="imgprofile"></img>
+        <h3 align="center">@nurmayunitasari</h3>
+</body>
+</html>
